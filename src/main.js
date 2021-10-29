fbe2e791428a8e5823d97d6541e5234d7634bb9d
Thomas Hayner <thom.hayner@gmail.com>
2021-10-29 17:27:56 -0600

    chrome.tabs API not connected yet, button shows though

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ const Main = () => {
   return (
     <div>
       <h1>Hello Browser Extension World!</h1>
+      <button
+        onClick={() => {
+          // chrome.tabs.getCurrent(tab => {
+          //   chrome.tabs.update(tab.id, {
+          //     url: 'chrome-search://local-ntp/local-ntp.html',
+          //   });
+          // });
+        }}>
+        Chrome Tab
+      </button>
     </div>
   )
 } 
