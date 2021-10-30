4be14fc594c9c37922f8c7f59aa99e6cc4379738
Thomas Hayner <thom.hayner@gmail.com>
2021-10-29 18:23:34 -0600

    uncomment chrome.tabs API references

--- src/main.js
+++ src/main.js
@@ -8,11 +8,11 @@ const Main = () => {
       <h1>Hello Browser Extension World!</h1>
       <button
         onClick={() => {
-          // chrome.tabs.getCurrent(tab => {
-          //   chrome.tabs.update(tab.id, {
-          //     url: 'chrome-search://local-ntp/local-ntp.html',
-          //   });
-          // });
+          chrome.tabs.getCurrent(tab => {
+            chrome.tabs.update(tab.id, {
+              url: 'chrome-search://local-ntp/local-ntp.html',
+            });
+          });
         }}>
         Chrome Tab
       </button>
