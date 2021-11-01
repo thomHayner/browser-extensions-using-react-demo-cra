47978b418ecfdc31f009f02ece0422e4d420c3f8
thomHayner <thom.hayner@gmail.com>
2021-10-31 23:23:17 -0600

    try to get working without router

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,9 +1,11 @@
-import PopupMain from "./popupMain";
+// import PopupMain from "./popupMain"; // this was used tomimic App.js to try and get it to work without react-router
+// import React from 'react'; // this has no effect, should not be necessary in this file anyway
 
 function Popup() {
   return (
-    <div className="Popup" >
-      <PopupMain/>
+    <div className="popup" >
+      <h1>Hello Browser Extension Popup World!</h1>
+      {/* <PopupMain/> */}
     </div>
   );
 }
