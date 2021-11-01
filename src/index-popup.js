962f972d904d2bb09db3cbeccea495afc33bf8e1
thomHayner <thom.hayner@gmail.com>
2021-10-31 21:08:05 -0600

    test multiple options to get popup menu to display w/o router

--- src/index-popup.js
+++ src/index-popup.js
@@ -1,7 +1,14 @@
 //  This is the initial render file
 import React from 'react';
-import { render } from 'react-dom';
+import { ReactDOM, render } from 'react-dom';
 import './index.css';
 import Popup from './components/Popup.js';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Popup />
+//   </React.StrictMode>,
+//   document.getElementById('popup')
+// );
 
 render(<Popup />, document.querySelector('#popup'));
