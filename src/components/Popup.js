b760809993adcd46befd59de810a808d4fa9a452
thomHayner <thom.hayner@gmail.com>
2021-11-01 12:39:25 -0600

    pushing to continue working remotely

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,11 +1,11 @@
-// import PopupMain from "./popupMain"; // this was used tomimic App.js to try and get it to work without react-router
+import PopupMain from "./popupMain"; // this was used to mimic App.js to try and get it to work without react-router
 // import React from 'react'; // this has no effect, should not be necessary in this file anyway
 
 function Popup() {
   return (
     <div className="popup" >
-      <h1>Hello Browser Extension Popup World!</h1>
-      {/* <PopupMain/> */}
+      {/* <h1>Hello Browser Extension Popup World!</h1> */}
+      <PopupMain/>
     </div>
   );
 }
